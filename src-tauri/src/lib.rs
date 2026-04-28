99435217753be0d19f563adada9c0cef848892c0
Euan <tictakt.app@gmail.com>
2026-04-28 09:48:54 +0100

    feat: [F-003] short description

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+﻿#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+        .plugin(tauri_plugin_sql::init())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -14,3 +15,4 @@ pub fn run() {
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
